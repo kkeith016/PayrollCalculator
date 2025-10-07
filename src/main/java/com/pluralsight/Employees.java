c2271bf4acf7bb3d8aa803f8c97756b1b11e4fd5
Kylee <kkeith.biz@gmail.com>
2025-10-07 09:56:01 -0400

    employees class

---
+++ src/main/java/com/pluralsight/Employees.java
@@ -0,0 +1,48 @@
+package com.pluralsight;
+
+public class Employees {
+
+    private int id;
+    private String fullName;
+    private double hours;
+    private double payRate;
+
+    public Employees(int id, String fullName, double hours, double payRate) {
+        this.id = id;
+        this.fullName = fullName;
+        this.hours = hours;
+        this.payRate = payRate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public double getHours() {
+        return hours;
+    }
+
+    public void setHours(double hours) {
+        this.hours = hours;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+}
